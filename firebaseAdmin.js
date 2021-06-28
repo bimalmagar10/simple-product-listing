440cc06a7c7e1a23953b4eb07a00bc2649be4b1f
bimalmagar10 <bimalmagar873@gmail.com>
2021-06-28 18:54:12 +0545

    added google private key

--- firebaseAdmin.js
+++ firebaseAdmin.js
@@ -1,21 +1,23 @@
 import * as admin from "firebase-admin";
- const service ={
- 	  "type":process.env.TYPE,
- 	  "project_id":process.env.PROJECT_ID,
- 	  "private_key_id":process.env.PRIVATE_KEY_ID,
- 	  "private_key":process.env.PRIVATE_KEY,
- 	  "client_email":process.env.CLIENT_EMAIL,
- 	  "client_id":process.env.CLIENT_ID,
- 	  "auth_uri":process.env.AUTH_URI,
- 	  "token_uri":process.env.TOKEN_URI,
- 	  "auth_provider_x509_cert_url":process.env.AUTH_PROVIDER_X509_CERT_URL,
- 	  "client_x509_cert_url":process.env.CLIENT_X509_CERT_URL
- };
+const atob = require('atob');
+const privateKey = atob(process.env.google_private_key);
+ // const service ={
+ // 	  "type":process.env.TYPE,
+ // 	  "project_id":process.env.PROJECT_ID,
+ // 	  "private_key_id":process.env.PRIVATE_KEY_ID,
+ // 	  "private_key":process.env.PRIVATE_KEY,
+ // 	  "client_email":process.env.CLIENT_EMAIL,
+ // 	  "client_id":process.env.CLIENT_ID,
+ // 	  "auth_uri":process.env.AUTH_URI,
+ // 	  "token_uri":process.env.TOKEN_URI,
+ // 	  "auth_provider_x509_cert_url":process.env.AUTH_PROVIDER_X509_CERT_URL,
+ // 	  "client_x509_cert_url":process.env.CLIENT_X509_CERT_URL
+ // };
 
 export const verifyIdToken = token => {
 	if(!admin.apps.length) {
         admin.initializeApp({
-		  credential: admin.credential.cert(serviceAccount),
+		  credential: admin.credential.cert(privateKey),
 		  databaseURL:process.env.DATABASE_URL
 		})
 	}
--- next.config.js
+++ next.config.js
@@ -4,23 +4,6 @@ module.exports = {
     domains:['postimg.cc'],
   },
   env: {
-      TYPE:process.env.TYPE,
-      PROJECT_ID:process.env.PROJECT_ID,
-      PRIVATE_KEY_ID:process.env.PRIVATE_KEY_ID,
-      PRIVATE_KEY:process.env.PRIVATE_KEY,
-      CLIENT_EMAIL:process.env.CLIENT_EMAIL,
-      CLIENT_ID:process.env.CLIENT_ID,
-      AUTH_URI:process.env.AUTH_URI,
-      TOKEN_URI:process.env.TOKEN_URI,
-      AUTH_PROVIDER_X509_CERT_URL:process.env.AUTH_PROVIDER_X509_CERT_URL,
-      CLIENT_X509_CERT_URL:process.env.CLIENT_X509_CERT_URL,
-      DATABASE_URL:process.env.DATABASE_URL,
-      NEXT_PUBLIC_APIKEY:process.env.NEXT_PUBLIC_APIKEY,
-      NEXT_PUBLIC_AUTHDOMAIN:process.env.NEXT_PUBLIC_AUTHDOMAIN,
-      NEXT_PUBLIC_PROJECTID:process.env.NEXT_PUBLIC_PROJECTID,
-      NEXT_PUBLIC_STORAGEBUCKET:process.env.NEXT_PUBLIC_STORAGEBUCKET,
-      NEXT_PUBLIC_MESSAGINGSENDERID:process.env.NEXT_PUBLIC_MESSAGINGSENDERID,
-      NEXT_PUBLIC_APPID:process.env.NEXT_PUBLIC_APPID,
-      NEXT_PUBLIC_STRIPE_KEY:process.env.NEXT_PUBLIC_STRIPE_KEY
+     "google_private_key": "@google-private-key"
   }
 }
